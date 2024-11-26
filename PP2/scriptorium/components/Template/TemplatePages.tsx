08968c9a234b4ea9625b0b041b07179d7735b168
fern1394 <fernandesjaron@gmail.com>
2024-11-26 05:40:22 -0500

    modified it to make sure that buttons were greyed out and disabled better

--- PP2/scriptorium/components/Template/TemplatePages.tsx
+++ PP2/scriptorium/components/Template/TemplatePages.tsx
@@ -10,17 +10,19 @@ interface TemplatePagesProps {
 const TemplatePages: React.FC<TemplatePagesProps> = ({ currentPage, totalPages, onPageChange }) => {
   const handlePrev = () => {
     if (currentPage > 1) onPageChange(currentPage - 1);
+    if (totalPages === 0) onPageChange(0);
   };
 
   const handleNext = () => {
     if (currentPage < totalPages) onPageChange(currentPage + 1);
+    if (totalPages === 0) onPageChange(0);
   };
 
   return (
     <div className="flex justify-center items-center space-x-4 my-4">
       <button
         onClick={handlePrev}
-        disabled={currentPage === 1}
+        disabled={currentPage === 1 || totalPages === 0}
         className={`px-4 py-2 border rounded ${
           currentPage === 1 ? 'text-gray-400 cursor-not-allowed' : 'text-blue-500 hover:bg-blue-100'
         }`}
@@ -28,13 +30,13 @@ const TemplatePages: React.FC<TemplatePagesProps> = ({ currentPage, totalPages, 
         Previous
       </button>
       <span>
-        Page {currentPage} of {totalPages}
+        Page {currentPage > totalPages ? 0 : currentPage} of {totalPages}
       </span>
       <button
         onClick={handleNext}
-        disabled={currentPage === totalPages}
+        disabled={currentPage === totalPages || totalPages === 0}
         className={`px-4 py-2 border rounded ${
-          currentPage === totalPages ? 'text-gray-400 cursor-not-allowed' : 'text-blue-500 hover:bg-blue-100'
+          (currentPage === totalPages || totalPages === 0) ? 'text-gray-400 cursor-not-allowed' : 'text-blue-500 hover:bg-blue-100'
         }`}
       >
         Next
