65c9afa90b6e0ff01dbaf69392fe2028f4ce4b84
fern1394 <fernandesjaron@gmail.com>
2024-11-27 05:55:59 -0500

    changed Run triangle to View triangle bc it makes more sense

--- PP2/scriptorium/components/Template/TemplateList.tsx
+++ PP2/scriptorium/components/Template/TemplateList.tsx
@@ -77,7 +77,7 @@ const RunButton = ({ template }: { template: Template }) => {
     <button 
       onClick={handleRunClick}
       className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-      Run ▶
+      View ▶
     </button>
   );
 }
