6bed0550f3a519d9ee1072169eb86fc919bc8be5
riyadvaliyev <riyadvy@gmail.com>
2024-11-27 03:35:43 -0500

    admin panel created for admins

--- PP2/scriptorium/components/Shared/Navbar.tsx
+++ PP2/scriptorium/components/Shared/Navbar.tsx
@@ -6,27 +6,30 @@ import { useRouter } from 'next/router';
 const Navbar: React.FC = () => {
   const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    // Check if the user is logged in based on the accessToken
     const token = localStorage.getItem('accessToken');
-    setIsLoggedIn(!!token); // If token exists, the user is logged in
+    const userRole = localStorage.getItem('userRole'); // Assuming the role is stored locally after login
+    console.log("role:", userRole);
+    setIsLoggedIn(!!token);
+    setIsAdmin(userRole === 'ADMIN');
   }, []);
 
   const handleLogout = () => {
-    // Clear tokens and redirect to home
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('userId');
+    localStorage.removeItem('userRole');
     setIsLoggedIn(false);
-    router.push('/'); // Redirect to home after logout
+    router.push('/');
   };
 
   const handleDashboardClick = () => {
     if (isLoggedIn) {
-      router.push('/dashboard'); // Redirect to Dashboard if logged in
+      router.push('/dashboard');
     } else {
-      router.push('/login'); // Redirect to Login if not logged in
+      router.push('/login');
     }
   };
 
@@ -47,6 +50,11 @@ const Navbar: React.FC = () => {
         <Link href="/editor" className="hover:underline">
           Code Editor
         </Link>
+        {isAdmin && (
+          <Link href="/admin/reports" className="hover:underline">
+            Admin Panel
+          </Link>
+        )}
         <button className="hover:underline" onClick={handleDashboardClick}>
           Dashboard
         </button>
